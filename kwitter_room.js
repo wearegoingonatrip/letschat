c9ce7fc0870a862ba0bfa1fc3d27429c5964f19a
wearegoingonatrip <108912256+wearegoingonatrip@users.noreply.github.com>
2022-07-30 11:39:07 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,9 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCZOY9H4EldOaMWNfLX-xc9-gauvUy0n-Q",
+    authDomain: "chatwme-29279.firebaseapp.com",
+    projectId: "chatwme-29279",
+    storageBucket: "chatwme-29279.appspot.com",
+    messagingSenderId: "1061317875728",
+    appId: "1:1061317875728:web:2e520e1b85d9574368d295"
+  };
